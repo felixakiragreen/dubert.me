6a8086024e15b89f40b6ffd6b46529185f463d3e
Luke Dubert <luke@bluebridgedigital.com>
2018-11-03 00:54:32 -0400

    update index text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,16 @@ class IndexPage extends React.Component {
       <main className={this.props.uiState}>
 
         <h1>Luke Dubert</h1>
-        <p>Luke is a Portuguese-born, Swedish American futurist, designer, developer, entrepreneur, visionary, & day-dreamer. Raised in southern Africa, he is baffled by the U.S.' resistance to Metrication.</p>
+        <p>Luke is a Portuguese-born, Swedish American futurist, designer, & developer. Raised in southern Africa, he is baffled by the U.S.' resistance to Metrication.</p>
 
         <article>
           <h2>Personal</h2>
-          <p>Awaiting a Tesla Model 3, his primary transportation is a Ducati Monster 797. In his free time he enjoys playing & building games, instagramming his cat, & developing fantastical ideas.</p>
+          <p>He enjoys a simple life, surrounded by his favorite things: a lady-person, a cat, and a Tesla Model 3. He enjoys playing Dead Cells, solving rubix cubes, day-dreaming, and working on outlandish ideas.</p>
         </article>
 
         <article>
           <h2>Work</h2>
-          <p>Currently employed full-time as a Front-end Engineer at <a href='https://emplify.com/'>Emplify</a>. He is on a team building a web portal that provides insight & data to leaders about their employees' engagement.</p>
+          <p>For love and money, he designs and builds websites; more specifically, interfaces for products that focus on enjoyable user experiences while being visually appealing.</p>
         </article>
 
       </main>
